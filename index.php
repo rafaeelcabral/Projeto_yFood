7f04ae57d0bb6f6328da5cba26dc997e4ce3d0fc
Rafael Cabral <melloraf.cabral@gmail.com>
2025-07-05 11:58:13 -0300

    altera nome da tittle do index

--- index.php
+++ index.php
@@ -39,7 +39,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>yFood - Lanchonete</title>
+    <title>yFood - Menu</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet">
 </head>
